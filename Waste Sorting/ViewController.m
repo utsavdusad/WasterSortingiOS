7f830afdc831c38e4bd71284980c2872f4ee5ef4
Utsav Dusad <udusad@asu.edu>
2018-10-01 22:31:22 -0700

    #43 Select existing image

--- Waste Sorting/ViewController.m
+++ Waste Sorting/ViewController.m
@@ -7,9 +7,10 @@
 //
 
 #import "ViewController.h"
+#import "QBImagePickerController.h"
 
-@interface ViewController ()
-
+@interface ViewController ()<QBImagePickerControllerDelegate>
+@property (strong, nonatomic) UIImage *image;
 @end
 
 @implementation ViewController
@@ -25,5 +26,61 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (IBAction)selectPhoto:(id)sender {
+    QBImagePickerController *imagePickerController = [QBImagePickerController new];
+    imagePickerController.delegate = self;
+    imagePickerController.mediaType = QBImagePickerMediaTypeImage;
+    imagePickerController.allowsMultipleSelection = 1;
+    imagePickerController.showsNumberOfSelectedAssets = YES;
+    imagePickerController.prompt = @"Select the photo you want to add!";
+    imagePickerController.maximumNumberOfSelection = 1;
+    [self presentViewController:imagePickerController animated:YES completion:nil];
+    
+}
+
+
+#pragma mark - QBImagePickerControllerDelegate
+//******************************************
+//Description: This method is called when the user is done selecting of images.
+- (void)qb_imagePickerController:(QBImagePickerController *)imagePickerController didFinishPickingAssets:(NSArray *)assets
+{
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+    
+    
+    __weak ViewController *weakSelf = self;
+    [assets enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+        PHAsset *asset=(PHAsset *)obj;
+        PHImageRequestOptions *requestOptions = [[PHImageRequestOptions alloc] init];
+        requestOptions.resizeMode   = PHImageRequestOptionsResizeModeExact;
+        requestOptions.deliveryMode = PHImageRequestOptionsDeliveryModeHighQualityFormat;
+        requestOptions.synchronous = YES;
+        
+        PHImageManager *manager = [PHImageManager defaultManager];
+        [manager requestImageForAsset:asset
+                           targetSize:PHImageManagerMaximumSize
+                          contentMode:PHImageContentModeDefault
+                              options:requestOptions
+                        resultHandler:^void(UIImage *image, NSDictionary *info) {
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                            weakSelf.imageView.image = image;
+                        weakSelf.image= image;
+                          });
+                            
+                        }];
+
+    }];
+    
+  
+    
+}
+
+- (void)qb_imagePickerControllerDidCancel:(QBImagePickerController *)imagePickerController
+{
+  
+    
+    [self dismissViewControllerAnimated:YES completion:NULL];
+}
 
 @end
