0a5cb5d690943fed86ce63e3c555f82795d6c983
Utsav Dusad <udusad@asu.edu>
2018-10-06 18:16:45 -0700

    Add server Path

--- Waste Sorting/Constants/Constants.h
+++ Waste Sorting/Constants/Constants.h
@@ -3,6 +3,7 @@
 
 #import <Foundation/Foundation.h>
 #define SERVER_ADDRESS @"http://wastesorting-backend.gpm22aep7i.us-west-1.elasticbeanstalk.com"
+#define SERVER_PATH @"http://wastesorting-backend.hyxs8yaymw.us-west-1.elasticbeanstalk.com/uploadImage"
 
 @interface Constants : NSObject
 
