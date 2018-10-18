d54fc7288f5bc23023d023508502eeaff23d54f2
Utsav Dusad <udusad@asu.edu>
2018-10-18 08:32:03 -0700

    Task #60Upload the image to the server

--- Waste Sorting/Constants/Constants.h
+++ Waste Sorting/Constants/Constants.h
@@ -3,7 +3,7 @@
 
 #import <Foundation/Foundation.h>
 #define SERVER_ADDRESS @"http://wastesorting-backend.gpm22aep7i.us-west-1.elasticbeanstalk.com"
-#define SERVER_PATH @"http://wastesorting-backend.hyxs8yaymw.us-west-1.elasticbeanstalk.com/uploadImage"
+#define SERVER_PATH @"http://wasteZero1.ndv2pn2ti5.us-east-2.elasticbeanstalk.com/getTrashCategory"
 
 @interface Constants : NSObject
 
